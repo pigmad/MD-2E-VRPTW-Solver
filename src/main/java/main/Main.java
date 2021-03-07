f1552525eba3f1314ff0af6814ee615f95891d2d
Dorian <lastennet.dorian@live.fr>
2021-03-07 18:58:50 +0100

    modification affichage

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,9 +7,8 @@ import solver.TestSolution;
 import utils.FileManager;
 import utils.FileManagerException;
 
-import java.io.File;   
+import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.JFileChooser;
 
@@ -26,13 +25,13 @@ public class Main {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Merci de choisir un fichier d'instance à lire");
         fileChooser.setCurrentDirectory(new File(System.getProperty("user.dir")));
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES","txt", "text");
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
         fileChooser.setFileFilter(filter);
         int result = fileChooser.showOpenDialog(null);
         if (result == JFileChooser.APPROVE_OPTION) {
             String instanceFile = fileChooser.getSelectedFile().getAbsolutePath();
             System.out.println("Vous avez sélectionné le fichier : " + instanceFile);
-            
+
             //lecture fichier et construction de l'instance
             FileManager fm = new FileManager(instanceFile);
             Instance instance = fm.readInstance();
@@ -42,17 +41,18 @@ public class Main {
             Solver solver = new Solver(instance);
             TestSolution solTest = new TestSolution();
             solver.setHeuristic(solTest);
-            
+
             //récuperation de la solution
             solver.solveInstance();
             Solution solution = solver.getSolution();
             System.out.println(solution);
 
-            System.out.println("Fonction Obj : "+solver.evaluateSolution(solution));
-            System.out.println("Contraintes temporelles respectées : "+(solver.isSecondEchelonTimeWindowsRespected(solution)?"oui":"non"));
-            System.out.println("Contraintes capacités respectées : "+(solver.isSecondEchelonCapacitiesRespected(solution)?"oui":"non"));
-        
-            fm.writeSolution(solver,instanceFile+"-result.txt");
-        }    
+            System.out.println("Fonction Obj : " + solver.evaluateSolution(solution));
+            System.out.println("Contraintes temporelles respectées : " + (solver.isSecondEchelonTimeWindowsRespected(solution) ? "oui" : "non"));
+            System.out.println("Contraintes capacités respectées : " + (solver.isSecondEchelonCapacitiesRespected(solution) ? "oui" : "non"));
+            System.out.println("Contraintes véhicules respectées : " + (solver.isSecondEchelonVehiclesNumberRespected(solution) ? "oui" : "non"));
+
+            fm.writeSolution(solver, instanceFile + "-result.txt");
+        }
     }
 }
